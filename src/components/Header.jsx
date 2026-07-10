0bba9233b9d2d4b4c71aed8241e821419f8f2a6f
dev_eduardo <fredyriscanevo1971@gmail.com>
2026-07-10 00:02:44 -0500

    feat: Add styling to the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'       // eslint-disable-line no-unused-vars
 export default function Header() {
   return (
     <header>
-        <h1>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Prisma</span>
             <span className='text-slate-700'>Inmobiliario</span>
         </h1>
