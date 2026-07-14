96382a2cec895275cee2677bee50a648a409000d
dev_eduardo <fredyriscanevo1971@gmail.com>
2026-07-13 21:48:07 -0500

    refactor: alert messages

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,10 +4,10 @@ import { errorHandler } from './error.js';
 
 export const verifyToken = async (req, res, next) => {
     const token = req.cookies.access_token;
-    if (!token) return res.status(401).json({ message: 'Unauthorized' });
+    if (!token) return res.status(401).json({ message: 'No autorizado' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return next(errorHandler(403, 'Forbidden'));
+        if (err) return next(errorHandler(403, '¡Acceso denegado!'));
         req.user = user;
         next();
     });
