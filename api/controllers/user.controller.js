e8cdbe75d81e69a4156c6e37c3133d8ec91658f2
dev_eduardo <fredyriscanevo1971@gmail.com>
2026-07-17 19:27:10 -0500

    feat:  Create an API endpoint to retrieve user listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
 
@@ -37,6 +38,16 @@ export const deleteUser = async (req, res, next) => {
         await User.findByIdAndDelete(req.params.id);
         res.clearCookie('access_token');
         res.status(200).json('Usuario eliminado correctamente') ;
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, '¡Solo puedes ver tus propios listados!'));
+    try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
     } catch (error) {
         next(error);
     }
